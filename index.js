bd11607d6c0e8c3d3cff9e069931167097e61b88
Brian Blackwell <industriousthought@gmail.com>
2014-10-06 08:43:20 +0000

    completed the demonstation tutorial of chat app

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 
-        res.sendFile('index.html');
+        res.sendFile('/home/industriousthought/Web/chat-example/index.html');
 
         });
 
@@ -18,6 +18,16 @@ io.on('connection', function(socket){
             console.log('user disconnected');
 
             });
+        });
+
+io.on('connection', function(socket){
+
+        socket.on('chat message', function(msg){
+
+            io.emit('chat message', msg);
+
+            });
+
         });
 
 http.listen(3000, function(){
